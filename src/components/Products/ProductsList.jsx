8bd46eef7d072eaca5fe80fbe0efb0da1c8497d6
Youngjae <2015fromjeki@gmail.com>
2024-07-05 22:08:05 +1000

    SECTION 12 Befroe 7

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -1,34 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./ProductsList.css";
 import ProductCard from "./ProductCard";
+import apiClient from "../../../utiles/api-client";
 
 const ProductsList = () => {
-    return (
-        <section className='products_list_section'>
-            <header className='align_center products_list_header'>
-                <h2>Products</h2>
-                <select name='sort' id='' className='products_sorting'>
-                    <option value=''>Relevance</option>
-                    <option value='price desc'>Price HIGH to LOW</option>
-                    <option value='price asc'>Price LOW to HIGH</option>
-                    <option value='rate desc'>Rate HIGH to LOW</option>
-                    <option value='rate asc'>Rate LOW to HIGH</option>
-                </select>
-            </header>
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState("");
+  const fetchData = async () => {
+    try {
+      const response = await apiClient.get("/products");
+      setProducts(response.data.products);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  console.log(products);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <section className="products_list_section">
+      <header className="align_center products_list_header">
+        <h2>Products</h2>
+        <select name="sort" id="" className="products_sorting">
+          <option value="">Relevance</option>
+          <option value="price desc">Price HIGH to LOW</option>
+          <option value="price asc">Price LOW to HIGH</option>
+          <option value="rate desc">Rate HIGH to LOW</option>
+          <option value="rate asc">Rate LOW to HIGH</option>
+        </select>
+      </header>
 
-            <div className='products_list'>
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-            </div>
-        </section>
-    );
+      <div className="products_list">
+        {error && <em className="error_message">{error}</em>}
+        {products.map((product) => (
+          <ProductCard key={product._id} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default ProductsList;
