862dd718c1e03d0c6a25e0a568c694f3bca18226
Youngjae <2015fromjeki@gmail.com>
2024-07-16 13:42:12 +1000

    Section13 (7/12)

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -4,71 +4,77 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import "./LoginPage.css";
+import { login } from "../../services/userServices";
 
 const schema = z.object({
-    email: z
-        .string()
-        .email({ message: "Please enter valid email address." })
-        .min(3),
-    password: z
-        .string()
-        .min(8, { message: "Password should be at least 8 characters." }),
+  email: z
+    .string()
+    .email({ message: "Please enter valid email address." })
+    .min(3),
+  password: z
+    .string()
+    .min(8, { message: "Password should be at least 8 characters." }),
 });
 
 const LoginPage = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({ resolver: zodResolver(schema) });
+  const [formError, setFormError] = useState("");
 
-    const onSubmit = (formData) => console.log(formData);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
 
-    return (
-        <section className='align_center form_page'>
-            <form
-                className='authentication_form'
-                onSubmit={handleSubmit(onSubmit)}>
-                <h2>Login Form</h2>
-                <div className='form_inputs'>
-                    <div>
-                        <label htmlFor='email'>Email</label>
-                        <input
-                            type='email'
-                            id='email'
-                            className='form_text_input'
-                            placeholder='Enter your email address'
-                            {...register("email")}
-                        />
-                        {errors.email && (
-                            <em className='form_error'>
-                                {errors.email.message}
-                            </em>
-                        )}
-                    </div>
-                    <div>
-                        <label htmlFor='password'>Password</label>
-                        <input
-                            type='password'
-                            id='password'
-                            className='form_text_input'
-                            placeholder='Enter your password'
-                            {...register("password")}
-                        />
-                        {errors.password && (
-                            <em className='form_error'>
-                                {errors.password.message}
-                            </em>
-                        )}
-                    </div>
-
-                    <button type='submit' className='search_button form_submit'>
-                        Submit
-                    </button>
-                </div>
-            </form>
-        </section>
-    );
+  const onSubmit = async (formData) => {
+    try {
+      login(formData);
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        setFormError(err.response.data.message);
+      }
+    }
+  };
+  console.log("HERE IS THE ERROR");
+  console.log(formError);
+  return (
+    <section className="align_center form_page">
+      <form className="authentication_form" onSubmit={handleSubmit(onSubmit)}>
+        <h2>Login Form</h2>
+        <div className="form_inputs">
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              className="form_text_input"
+              placeholder="Enter your email address"
+              {...register("email")}
+            />
+            {errors.email && (
+              <em className="form_error">{errors.email.message}</em>
+            )}
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              className="form_text_input"
+              placeholder="Enter your password"
+              {...register("password")}
+            />
+            {errors.password && (
+              <em className="form_error">{errors.password.message}</em>
+            )}
+          </div>
+          {formError && <em>{formError}</em>}
+          <button type="submit" className="search_button form_submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default LoginPage;
--- src/components/Authentication/SignupPage.jsx
+++ src/components/Authentication/SignupPage.jsx
@@ -5,6 +5,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import "./SignupPage.css";
 import user from "../../assets/user.webp";
+import { signup } from "../../services/userServices";
 
 const schema = z
   .object({
@@ -27,15 +28,22 @@ const schema = z
 
 const SignupPage = () => {
   const [profilePic, setProfilePic] = useState(null);
+  const [formError, setFormError] = useState("");
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({ resolver: zodResolver(schema) });
 
-  const onSubmit = (formData) => console.log(formData);
-
-  console.log(profilePic);
+  const onSubmit = async (formData) => {
+    try {
+      await signup(formData, profilePic);
+    } catch (err) {
+      if (err.response && err.response.status === 400) {
+        setFormError(err.message);
+      }
+    }
+  };
 
   return (
     <section className="align_center form_page">
@@ -134,7 +142,7 @@ const SignupPage = () => {
             )}
           </div>
         </div>
-
+        {formError && <em className="error">{formError}</em>}
         <button className="search_button form_submit" type="submit">
           Submit
         </button>
